f5393954552e189ff661016c2db49789a7da55a3
Karl Lew <karl@firepick.org>
2018-01-16 12:13:13 -0800

    Variable

--- src/Variable.js
+++ src/Variable.js
@@ -3,53 +3,55 @@ var mathjs = require("mathjs");
 (function(exports) {
 
     // CLASS
-    function Variable(values, distribution = Variable.UNIFORM) {
-        var that = this;
-        that.max = mathjs.max(values);
-        that.min = mathjs.min(values);
-        that.values = Object.assign([], values);
-        that.distribution = distribution;
-        if (distribution === Variable.UNIFORM) {
-            that.sample = Variable.prototype.sampleUniform;
-            that.mean = that.median = (that.min + that.max) / 2;
-        } else if (distribution === Variable.DISCRETE) {
-            that.sample = Variable.prototype.sampleDiscrete;
-            that.median = mathjs.median(that.values);
-            that.mean = mathjs.mean(that.values);
-        } else if (distribution === Variable.GAUSSIAN) {
-            that.sample = Variable.prototype.sampleGaussian;
-            that.median = (that.min + that.max) / 2;
-            that.sigma = that.max - that.min;
-            that.data = [];
-        } else {
-            throw new Error("Variable has unknown distribution:" + distribution);
-        }
-        return that;
-    }
-    Variable.prototype.sampleUniform = function() {
-        var that = this;
-        return mathjs.random(that.min, that.max);
-    }
-    Variable.prototype.sampleDiscrete = function() {
-        var that = this;
-        return mathjs.pickRandom(that.values);
-    }
-    Variable.prototype.sampleGaussian = function() {
-        var that = this;
-        if (that.data.length === 0) {
-            var N = 25;
-            for (var i = N; i-- > 0;) { // generate N*2 numbers
-                do {
-                    var x1 = 2.0 * mathjs.random() - 1.0;
-                    var x2 = 2.0 * mathjs.random() - 1.0;
-                    var w = x1 * x1 + x2 * x2;
-                } while (w >= 1.0);
-                w = mathjs.sqrt((-2.0 * mathjs.log(w)) / w);
-                that.data.push(x1 * w * that.sigma + that.median);
-                that.data.push(x2 * w * that.sigma + that.median);
+    class Variable {
+        constructor (values, distribution = Variable.UNIFORM) {
+            var that = this;
+            that.max = mathjs.max(values);
+            that.min = mathjs.min(values);
+            that.values = Object.assign([], values);
+            that.distribution = distribution;
+            if (distribution === Variable.UNIFORM) {
+                that.sample = Variable.prototype.sampleUniform;
+                that.mean = that.median = (that.min + that.max) / 2;
+            } else if (distribution === Variable.DISCRETE) {
+                that.sample = Variable.prototype.sampleDiscrete;
+                that.median = mathjs.median(that.values);
+                that.mean = mathjs.mean(that.values);
+            } else if (distribution === Variable.GAUSSIAN) {
+                that.sample = Variable.prototype.sampleGaussian;
+                that.median = (that.min + that.max) / 2;
+                that.sigma = that.max - that.min;
+                that.data = [];
+            } else {
+                throw new Error("Variable has unknown distribution:" + distribution);
             }
+            return that;
         }
-        return that.data.pop();
+        sampleUniform() {
+            var that = this;
+            return mathjs.random(that.min, that.max);
+        }
+        sampleDiscrete() {
+            var that = this;
+            return mathjs.pickRandom(that.values);
+        }
+        sampleGaussian() {
+            var that = this;
+            if (that.data.length === 0) {
+                var N = 25;
+                for (var i = N; i-- > 0;) { // generate N*2 numbers
+                    do {
+                        var x1 = 2.0 * mathjs.random() - 1.0;
+                        var x2 = 2.0 * mathjs.random() - 1.0;
+                        var w = x1 * x1 + x2 * x2;
+                    } while (w >= 1.0);
+                    w = mathjs.sqrt((-2.0 * mathjs.log(w)) / w);
+                    that.data.push(x1 * w * that.sigma + that.median);
+                    that.data.push(x2 * w * that.sigma + that.median);
+                }
+            }
+            return that.data.pop();
+        }
     }
 
     ///// CLASS
